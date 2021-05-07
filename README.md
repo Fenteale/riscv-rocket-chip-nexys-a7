51736290e743c79079339424441d2bd5ca13fd57
Mangmasta <mangmasta@gmail.com>
2021-05-06 21:27:31 -0700

    Added fverilog files generated from rocket-chip, and a README.

---
+++ README.md
@@ -0,0 +1,3 @@
+# RISCV Processor running on the Nexys A7
+
+Riscv verilog generated from [rocket-chip from chipsalliance](https://github.com/chipsalliance/rocket-chip).
---
+++ generated-src/freechips.rocketchip.system.DefaultConfig/firrtl_black_box_resource_files.f
@@ -0,0 +1,3 @@
+/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfig/SimDTM.cc
+/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfig/SimDTM.v
+/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultConfig/plusarg_reader.v
