c379bdca73ff9da9b2d173b10a4b2aa6bc0e3ad7
Mangmasta <mangmasta@gmail.com>
2021-05-10 21:00:13 -0700

    Added DefaultSmallConfig generated files.

---
+++ generated-src/freechips.rocketchip.system.DefaultSmallConfig/firrtl_black_box_resource_files.f
@@ -0,0 +1,3 @@
+/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultSmallConfig/SimDTM.cc
+/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultSmallConfig/SimDTM.v
+/rocket-chip/vsim/generated-src/freechips.rocketchip.system.DefaultSmallConfig/plusarg_reader.v
